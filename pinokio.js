0ef016b57044d25c80db111a5fb28b1adfe5d996
Nick Shykula <143186901+Shyk92@users.noreply.github.com>
2024-02-11 14:44:32 -0700

    Update pinokio.js

--- pinokio.js
+++ pinokio.js
@@ -1,56 +1,42 @@
-
+const os = require('os')
+const fs = require('fs')
+const path = require("path")
+const exists = (filepath) => {
+  return new Promise(r=>fs.access(filepath, fs.constants.F_OK, e => r(!e)))
+}
 
 module.exports = {
   title: "XTTS-RVC",
   description: "A Gradio UI for XTTSv2 and RVC, allowing for real-time voice conversion.",
   icon: "icon.jpg",
   menu: async (kernel) => {
-    let installed = await kernel.exists(__dirname, "app", "venv")
-    let installing = await kernel.running(__dirname, "install.js")
-    if (installing) {
+    let installed = await exists(path.resolve(__dirname, "env"))
+    if (installed) {
+      let session = (await kernel.loader.load(path.resolve(__dirname, "session.json"))).resolved
       return [{
-        icon: "fa-solid fa-plug",
-        text: "Installing",
-        href: "install.js",
+        when: "start.json",
+        on: "<i class='fa-solid fa-spin fa-circle-notch'></i> Running",
+        type: "label",
+        href: "start.json"
+      }, {
+        when: "start.json",
+        off: "<i class='fa-solid fa-power-off'></i> Launch",
+        href: "start.json?fullscreen=true&run=true",
+      }, {
+        when: "start.json",
+        on: (session && session.url ? "<i class='fa-solid fa-rocket'></i> Open Web UI" : null),
+        href: (session && session.url ? session.url : null),
+        target: "_blank"
+      }, {
+        when: "start.json",
+        on: "<i class='fa-solid fa-desktop'></i> Server",
+        href: "start.json?fullscreen=true"
       }]
-    } else if (installed) {
-      let session = await kernel.require(__dirname, "session.json")
-      let running = await kernel.running(__dirname, "start.json")
-      if (running) {
-        if (session && session.url) {
-          return [{
-            icon: "fa-solid fa-rocket",
-            text: "Open UI",
-            href: session.url,
-          }, {
-            icon: 'fa-solid fa-terminal',
-            text: "Terminal",
-            href: "start.json",
-          }]
-        } else {
-          return [{
-            icon: 'fa-solid fa-terminal',
-            text: "Terminal",
-            href: "start.json",
-          }]
-        }
-      } else {
-        return [{
-          icon: "fa-solid fa-power-off",
-          text: "Start",
-          href: "start.json",
-        }, {
-          icon: "fa-solid fa-plug",
-          text: "Install",
-          href: "install.js",
-        }]
-      }
     } else {
       return [{
-        icon: "fa-solid fa-plug",
-        text: "Install",
-        href: "install.json",
-        params: { run: true, fullscreen: true }
+        html: '<i class="fa-solid fa-plug"></i> Install',
+        type: "link",
+        href: "install.json?run=true&fullscreen=true"
       }]
     }
   }
