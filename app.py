42a408b2ce13ef58eaf5e3e586ed26552ea4bc35
Nick Shykula <143186901+Shyk92@users.noreply.github.com>
2024-02-13 12:44:14 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
+import sys
+sys.path.append('/XTTS-RVC-UI')
 import torch
 from TTS.api import TTS
 import gradio as gr
 from rvc import Config, load_hubert, get_vc, rvc_infer
-import gc , os, sys, argparse, requests
+import gc, os, sys, argparse, requests
 from pathlib import Path
 
+# Argument parsing setup
 parser = argparse.ArgumentParser(
 	prog='XTTS-RVC-UI',
 	description='Gradio UI for XTTSv2 and RVC'
@@ -17,53 +20,57 @@ if args.silent:
 	print('Enabling silent mode.')
 	sys.stdout = open(os.devnull, 'w')
 
+# Function to download required models
 def download_models():
+	# Download RVC models
 	rvc_files = ['hubert_base.pt', 'rmvpe.pt']
-
 	for file in rvc_files: 
-		if(not os.path.isfile(f'./models/{file}')):
-			print(f'Downloading{file}')
+		if not os.path.isfile(f'./models/{file}'):
+			print(f'Downloading {file}')
 			r = requests.get(f'https://huggingface.co/lj1995/VoiceConversionWebUI/resolve/main/{file}')
 			with open(f'./models/{file}', 'wb') as f:
-					f.write(r.content)
+				f.write(r.content)
 
+	# Download XTTS models
 	xtts_files = ['vocab.json', 'config.json', 'dvae.path', 'mel_stats.pth', 'model.pth']
-
 	for file in xtts_files:
-		if(not os.path.isfile(f'./models/xtts/{file}')):
+		if not os.path.isfile(f'./models/xtts/{file}'):
 			print(f'Downloading {file}')
 			r = requests.get(f'https://huggingface.co/coqui/XTTS-v2/resolve/v2.0.2/{file}')
 			with open(f'./models/xtts/{file}', 'wb') as f:
 				f.write(r.content)
-				
 
+# Create necessary directories
 [Path(_dir).mkdir(parents=True, exist_ok=True) for _dir in ['./models/xtts', './voices', './rvcs']]
-
 download_models()
 
-device = "cuda:0" if torch.cuda.is_available() else "cpu"
-if not torch.cuda.is_available() and torch.backends.mps.is_available():
-    device = "mps"
-print("Device: " + device) 
+# Device setup
+cuda_available = torch.cuda.is_available()
+mps_available = torch.backends.mps.is_available()
 
-config = Config(device, device != 'cpu')
-hubert_model = load_hubert(device, config.is_half, "./models/hubert_base.pt")
-tts = TTS(model_path="./models/xtts", config_path='./models/xtts/config.json').to(device)
+# Choose devices based on availability
+tts_device = "cuda:0" if cuda_available else "cpu"
+rvc_device = "mps" if mps_available else tts_device	 # Use MPS for RVC if available, otherwise fallback to the TTS device
+
+print(f"TTS Device: {tts_device}")
+print(f"RVC Device: {rvc_device}")
+
+config = Config(rvc_device, rvc_device != 'cpu')
+hubert_model = load_hubert(rvc_device, config.is_half, "./models/hubert_base.pt").to(rvc_device)
+tts = TTS(model_path="./models/xtts", config_path='./models/xtts/config.json').to(tts_device)
 voices = []
 rvcs = []
 langs = ["en", "es", "fr", "de", "it", "pt", "pl", "tr", "ru", "nl", "cs", "ar", "zh-cn", "hu", "ko", "ja", "hi"]
 
 def get_rvc_voices():
-	global voices 
 	voices = os.listdir("./voices")
-	global rvcs
-	rvcs = list(filter(lambda x:x.endswith(".pth"), os.listdir("./rvcs")))
-	return [rvcs, voices]
+	rvcs = list(filter(lambda x: x.endswith(".pth"), os.listdir("./rvcs")))
+	return rvcs, voices
 
 def runtts(rvc, voice, text, pitch_change, index_rate, language): 
-    audio = tts.tts_to_file(text=text, speaker_wav="./voices/" + voice, language=language, file_path="./output.wav")
-    voice_change(rvc, pitch_change, index_rate)
-    return ["./output.wav" , "./outputrvc.wav"]
+	audio = tts.tts_to_file(text=text, speaker_wav="./voices/" + voice, language=language, file_path="./output.wav")
+	voice_change(rvc, pitch_change, index_rate)
+	return ["./output.wav" , "./outputrvc.wav"]
 
 def main():
 	get_rvc_voices()
@@ -85,7 +92,7 @@ def main():
 				with gr.Row():
 					pitch_slider = gr.Slider(minimum=-12, maximum=12, value=0, step=1, label="Pitch")
 					index_rate_slider = gr.Slider(minimum=0, maximum=1, value=0.75, step=0.05, label="Index Rate")
-			with gr.Column():        
+			with gr.Column():		 
 				audio_output = gr.Audio(label="TTS result", type="filepath", interactive=False)
 				rvc_audio_output = gr.Audio(label="RVC result", type="filepath", interactive=False)
 
@@ -131,7 +138,7 @@ def voice_change(rvc, pitch_change, index_rate):
 	#load_cpt(modelname, rvc_model_path)
 	#cpt, version, net_g, tgt_sr, vc = get_vc(device, config.is_half, config, rvc_model_path)
 	rvc_data.load_cpt(modelname, rvc_model_path)
-	
+
 	rvc_infer(
 		index_path=rvc_index_path, 
 		index_rate=index_rate, 
@@ -151,6 +158,6 @@ def voice_change(rvc, pitch_change, index_rate):
 		hubert_model=hubert_model
 	)
 	gc.collect()
-    
+	
 if __name__ == "__main__":
-    main()
+	main()
